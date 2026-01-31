8c8304c2da0da312a854b12610988c245bdbad54
DESKTOP-C6L7PJB\てる <tdmamedash0318@gmail.com>
2026-01-31 10:27:13 +0900

    思い出しつつ、メニューアイテムを配置。

---
+++ TDOfficeToolkit/Views/VersionView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using YKToolkit.Controls;
+
+namespace TDOfficeToolkit.Views;
+
+/// <summary>
+/// VersionView.xaml の相互作用ロジック
+/// </summary>
+public partial class VersionView : Window
+{
+  public VersionView()
+  {
+    InitializeComponent();
+  }
+}
