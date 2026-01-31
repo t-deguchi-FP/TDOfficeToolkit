c1b8c4b92935807e2d06f2339b2cd1614de550cc
DESKTOP-C6L7PJB\てる <tdmamedash0318@gmail.com>
2026-01-31 18:58:04 +0900

    メインビューの見た目変更。

--- TDOfficeToolkit/ViewModels/MainViewModel.cs
+++ TDOfficeToolkit/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using YKToolkit.Bindings;
+﻿using System.Windows;
+using YKToolkit.Bindings;
 
 namespace TDOfficeToolkit.ViewModels;
 
@@ -6,4 +7,25 @@ internal class MainViewModel : NotificationObject
 {
   // バージョン情報表示コマンド
   public DelegateCommand ShowVersionCommand => new DelegateCommand(_ => App.ShowVersionWindow());
+
+  // Knighthead 申込書作成を開くコマンド
+  public DelegateCommand OpenKnightheadCommand => new DelegateCommand(_ => OpenKnightheadTool());
+
+  // 確認書作成を開くコマンド
+  public DelegateCommand OpenConfirmationCommand => new DelegateCommand(_ => OpenConfirmationTool());
+
+  // 終了コマンド
+  public DelegateCommand ExitCommand => new DelegateCommand(_ => Application.Current.Shutdown());
+
+  private void OpenKnightheadTool()
+  {
+    // TODO: Knighthead用のウィンドウまたはビューを表示
+    MessageBox.Show("Knighthead 申込書作成を開きます", "情報", MessageBoxButton.OK, MessageBoxImage.Information);
+  }
+
+  private void OpenConfirmationTool()
+  {
+    // TODO: 確認書作成用のウィンドウまたはビューを表示
+    MessageBox.Show("確認書作成を開きます", "情報", MessageBoxButton.OK, MessageBoxImage.Information);
+  }
 }
